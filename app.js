ddab96f59014d1f2f2cfa8fe7f6a901f9e2b494d
smk87 <sqlnmhmdkhanemon@gmail.com>
2019-08-23 12:01:21 +0600

    Attack method added.

--- app.js
+++ app.js
@@ -4,5 +4,32 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false
+  },
+  methods: {
+    startGame() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack() {
+      let max = 10;
+      let min = 3;
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert("You won!");
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+    },
+    specialAttack() {},
+    heal() {},
+    giveUp() {}
   }
 });
